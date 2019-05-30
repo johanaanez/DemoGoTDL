70415d1e17dbe36a0582db053a8d7d483e22a1c5
Federico Di Rocco <federico.dirocco@wolox.com.ar>
2019-05-29 21:06:41 -0300

    Fixed tabs

--- example-simple-blank/bankTransaction.go
+++ example-simple-blank/bankTransaction.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-		"fmt"
-		"sync"
+	"fmt"
+	"sync"
 )
 
 var (
-		balance int
-		mutex	sync.Mutex
+	balance int
+	mutex	sync.Mutex
 )
 
 func init() {
-    balance = 1000
+	balance = 1000
 }
 
 func deposit(index int, value int, wg *sync.WaitGroup) {
-		//mutex.Lock()
-		balance += value
-		fmt.Printf("%d) Deposito: %d. Balance en la cuenta: %d\n", index, value, balance)
-		wg.Done()
-		//mutex.Unlock()
+	//mutex.Lock()
+	balance += value
+	fmt.Printf("%d) Deposito: %d. Balance en la cuenta: %d\n", index, value, balance)
+	wg.Done()
+	//mutex.Unlock()
 }
 
 func extract(index int, value int, wg *sync.WaitGroup) {
-		//mutex.Lock()
-		balance -= value
-		fmt.Printf("%d) Extraccion: %d. Balance en la cuenta: %d\n", index, value, balance)
-		wg.Done()
-		//mutex.Unlock()
+	//mutex.Lock()
+	balance -= value
+	fmt.Printf("%d) Extraccion: %d. Balance en la cuenta: %d\n", index, value, balance)
+	wg.Done()
+	//mutex.Unlock()
 }
 
 func main() {
-    fmt.Println("¡Bienvenidos a GObank!")
+	fmt.Println("¡Bienvenidos a GObank!")
 
-		var w sync.WaitGroup
-    for i := 1; i <= 100; i++ {
-        w.Add(2)        
-        go extract(i, 200, &w)
-    		go deposit(i, 200, &w)
-		}		
-    w.Wait()
+	var w sync.WaitGroup
+	for i := 1; i <= 100; i++ {
+		w.Add(2)        
+		go extract(i, 200, &w)
+		go deposit(i, 200, &w)
+	}		
+	w.Wait()
 
-    fmt.Printf("Balance final: %d\n", balance)
+	fmt.Printf("Balance final: %d\n", balance)
 }
